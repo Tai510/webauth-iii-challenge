6955d2270a5e20bb1dc0b4f168d2857f62c05baf
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-06-12 15:12:17 -0700

    created login and token

--- api/server.js
+++ api/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const helmet = require('helmet');
+//const cors = require('cors');
 
-
+const authRouter = require('../auth/auth-router.js')
 
 const server = express();
-
+//server.use(cors());
 server.use(helmet());
 server.use(express.json());
 
+
+server.use('/api/auth', authRouter);
+
+// server.get('/', (req, res) => {
+//     res.send("It's alive!");
+//   });
 
 module.exports = server;
